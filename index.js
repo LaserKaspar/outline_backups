e870459a691a703e7a7e06fd2f4e1213087045f0
Felix Kaspar <ich@felixkaspar.com>
2023-09-15 01:10:01 +0200

    download file & cleanup export

--- index.js
+++ index.js
@@ -1,33 +1,77 @@
-import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+// Web Api
+import fetch from 'node-fetch';
 
+// File Download
+import fs from 'fs';
+import http from 'http';
+
+// Date Format
+const DATE = formatDate(new Date()); // YY-mm-dd_HH-MM-SS 
+
+// Start Export
 (async () => {
-    const getExport = await fetch(process.env.OUTLINE_URL + "/api/collections.export_all", {
+    const collectionUrl = process.env.FILES == "all" ? "/api/collections.export_all" : "/api/collections.export";
+    const getExport = await fetch(process.env.OUTLINE_URL + collectionUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: 'Bearer ' + process.env.OUTLINE_KEY
+            "Authorization": 'Bearer ' + process.env.OUTLINE_KEY
         },
-        body: JSON.stringify({ })
+        body: JSON.stringify({
+            "format": process.env.FORMAT,
+            "id": process.env.FILES
+        })
     });
 
+    if(getExport.status == 400) {
+        console.log("Bad Request, did you specify the correct .env.FILES? " + process.env.FILES);
+        return
+    }
     const exported = (await getExport.json()).data;
 
+    console.log("Starting Export...");
     await awaitExport(exported.fileOperation.id);
 
     const getDownloadUrl = await fetch(process.env.OUTLINE_URL + "/api/fileOperations.redirect", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: 'Bearer ' + process.env.OUTLINE_KEY
+            "Authorization": 'Bearer ' + process.env.OUTLINE_KEY
         },
         body: JSON.stringify({ id: exported.fileOperation.id })
     });
+    console.log("Export Done");
 
-    console.log(getDownloadUrl.url);
+    // Download File
+    console.log("Starting Download...");
+    const file = fs.createWriteStream(`./backups/outline-${DATE}_${process.env.FORMAT}.zip`);
+    http.get(getDownloadUrl.url, function(response) {
+        response.pipe(file);
+
+        // after download completed close filestream
+        file.on("finish", async () => {
+            file.close();
+            console.log("Download Completed");
+
+            console.log("Cleaning up...");
+            // Delete Export
+            if (process.env.DELETE_EXPORT_AFTER_DOWNLOAD) {
+                await fetch(process.env.OUTLINE_URL + "/api/fileOperations.delete", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: 'Bearer ' + process.env.OUTLINE_KEY
+                    },
+                    body: JSON.stringify({ id: exported.fileOperation.id })
+                });
+                console.log("Deleted Remote");
+            }
+            console.log("Done (:");
+        });
+    });
 })();
 
 async function awaitExport(fileOperationId) {
@@ -44,11 +88,24 @@ async function awaitExport(fileOperationId) {
             });
         
             const downloaded = await getDownload.json();
-    
+
             if(downloaded.data.state != "creating") {
                 resolve(downloaded)
                 break;
             }
         }
     });
+}
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function formatDate(date) {
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, '0');      // "+ 1" becouse the 1st month is 0
+    var day = String(date.getDate()).padStart(2, '0');
+    var hour = String(date.getHours()).padStart(2, '0');
+    var minutes = String(date.getMinutes()).padStart(2, '0');
+    var secconds = String(date.getSeconds()).padStart(2, '0');
+
+    return year + "-" + month + '-' + day + '_'+ hour+ '-'+ minutes+ '-'+ secconds;
 }
